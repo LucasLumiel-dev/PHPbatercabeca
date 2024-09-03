734a28c0f4d739c0c5e8be1a795871c08420d547
LucasLumiel-dev <rafaelasura1678@gmail.com>
2024-09-03 20:14:02 -0300

    Create exercitation.php

---
+++ exercitation.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bater_cabeca</title>
+    <meta name="authro" content="Lucas Macedo">
+    <header><h1>PHP ativ</h1></header>
+</head>
+<body>
+    <h1>exercicio 1</h1>
+    <form action="exercitation.php" method="post">
+        <label>Qual idade tu tem?</label>
+        <input type="text" name="valor1" required minlength="3">
+        <br>
+        <button type="submit" name="botao"> enviar </button>
+    </form>
+    
+</body>
+</html>
